5c6dc49b7de9fc8f98b482984fcd1743188718a7
ercekal <ercekalabalikoglu@gmail.com>
2016-11-23 17:13:07 +0000

    sets up hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,25 @@
 module.exports = {
   entry: [
-    './app/index.js'
+    './main.js'
   ],
+  output: {
+    path: './',
+    filename: "index.js"
+  },
+  devServer: {
+    inline: true,
+    port: 3000
+  },
   module: {
-    loaders: []
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
   }
 };
