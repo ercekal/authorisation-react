0cc214ffadf48a4a3784f0dab9cc810aa2c5664f
ercekal <ercekalabalikoglu@gmail.com>
2016-11-24 16:00:51 +0000

    mounting and unmounting elements

--- App.js
+++ App.js
@@ -3,48 +3,48 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    };
+    this.state = {val: 0};
     this.update = this.update.bind(this);
   }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-    });
+  update(){
+    this.setState({val: this.state.val +1 })
   }
-  render(){
+
+  componentWillMount() {
+    console.log('mounting')
+  }
+  render() {
+    console.log('rendering!')
+    return <button onClick={this.update}>{this.state.val}</button>
+  }
+  componentDidMount() {
+    console.log('mounted')
+  }
+  componentWillUnmount(){
+    console.log('bye')
+  }
+}
+
+
+class Wrapper extends React.Component {
+  constructor(){
+    super();
+  }
+  mount(){
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+  render () {
     return (
       <div>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br />
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a"></div>
       </div>
-    );
+    )
   }
 }
 
-class Slider extends React.Component {
-  render(){
-    return (
-      <div>
-      <input ref="inp" type="range"
-        min="0"
-        max="255"
-        onChange={this.props.update} />
-        </div>
-    );
-  }
-}
-
-export default App
+export default Wrapper
