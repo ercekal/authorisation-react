32929b3b9e6391b84f9a8232401f8f045f373463
ercekal <ercekalabalikoglu@gmail.com>
2016-11-28 21:59:35 +0000

    minor change but still doesn't work

--- App.js
+++ App.js
@@ -7,11 +7,11 @@ class App extends React.Component {
       {id: 3, name: "Liv Sib"},{id: 4, name: "sofi Sof"},
       {id: 5, name: "Mic Gek"},{id: 6, name: "Vaya vay"},
       {id: 7, name: "Ber Kay"},{id: 8, name: "Ece kal"}
-    ]}
+    ]};
   }
   render(){
     let rows = this.state.data.map( person => {
-      return <PersonRow data={person} />
+      return <PersonRow key={person.id} data={person} />
     })
     return
       <table>
