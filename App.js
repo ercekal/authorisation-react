f743a134ac64e7df0a194cb180639ad5815f8c37
ercekal <ercekalabalikoglu@gmail.com>
2016-11-28 21:56:02 +0000

    using map to create components from arrays.ts from Arrays. However it didn't work on my server as in the tutorial

--- App.js
+++ App.js
@@ -1,72 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {
-      red: 0
-    };
-    this.update = this.update.bind(this);
-  }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
-    });
+    this.state = {data: [
+      {id: 1, name: "Xav Bal"},{id: 2, name: "Erce Kal"},
+      {id: 3, name: "Liv Sib"},{id: 4, name: "sofi Sof"},
+      {id: 5, name: "Mic Gek"},{id: 6, name: "Vaya vay"},
+      {id: 7, name: "Ber Kay"},{id: 8, name: "Ece kal"}
+    ]}
   }
   render(){
-    return (
-      <div>
-        <NumInput
-        ref="red"
-        min = {0}
-        max={255}
-        step={0.01}
-        val={+this.state.red}
-        
-        label="Red"
-        update={this.update} />
-        <br />
-      </div>
-    );
+    let rows = this.state.data.map( person => {
+      return <PersonRow data={person} />
+    })
+    return
+      <table>
+        <tbody>{rows}</tbody>
+      </table>
   }
 }
 
-class NumInput extends React.Component {
-  render(){
-    let label = this.props.label !== '' ?
-      <label>{this.props.label} - {this.props.val}</label> : ''
-    return (
-      <div>
-      <input ref="inp"
-        type = {this.props.type}
-        min = {this.props.min}
-        max = {this.props.max}
-        step = {this.props.step}
-        defaultValue = {this.props.val}
-        onChange = {this.props.update} />
-        {label}
-        </div>
-    );
-  }
-}
-
-NumInput.propTypes = {
-  min : React.PropTypes.number,
-  max : React.PropTypes.number,
-  step : React.PropTypes.number,
-  val : React.PropTypes.number,
-  label : React.PropTypes.string,
-  min : React.PropTypes.func.isRequired,
-  type: React.PropTypes.oneOf(['number', 'range'])
-}
-
-NumInput.defaultProps = {
-  min : 0,
-  max : 0,
-  step : 1,
-  val : 0,
-  label : '',
-  type: 'range'
+const PersonRow = (props) => {
+  return <tr>
+    <td>{props.data.id}</td>
+    <td>{props.data.name}</td>
+    </tr>
 }
 
 export default App
