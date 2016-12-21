068167c64b98c64a4ac143e19136fbf5bf1d4f9c
ercekal <ercekalabalikoglu@gmail.com>
2016-12-21 16:01:51 +0000

    sets up the server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const http = require('http');
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:auth/erce');
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
